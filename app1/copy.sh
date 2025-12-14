1060f484981cf2643e65b7816323405cdf89a0c3
acc-vcc <acc_vcc@yahoo.co.jp>
2025-12-14 12:14:14 +0900

    Update copy.sh

--- app1/copy.sh
+++ app1/copy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# 成果物をまとめるディレクトリ
-mkdir -p output/app1
+# 成果物をまとめるディレクトリ(/appはマウントディレクトリ)
+mkdir -p /app/output/app1
 
 # 必要な成果物をコピー
-cp -p bin/testApp output/app1/bin/
+cp -p bin/testApp /app/output/app1/bin/
